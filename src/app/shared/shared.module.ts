ac39f3545c5bf37b852a73b7b54aab69e427a8ad
Félix <felix.szmorales@gmail.com>
2024-08-03 22:19:05 -0600

    Update inicio component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,19 +2,21 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import {MatIconModule} from '@angular/material/icon';
+import { InicioComponent } from './inicio/inicio.component';
 
 
 
 
 @NgModule({
   declarations: [
-HeaderComponent
+  HeaderComponent,
+  InicioComponent
   ],
   imports: [
     CommonModule,
     MatIconModule
   ],
-  exports:[HeaderComponent
+  exports:[HeaderComponent, InicioComponent
   ]
 })
 export class SharedModule { }
